7caaf610f2195fc0d218b4133d47c3cf33f36e96
Timothy Cherney <tjcherney@hotmail.com>
2025-07-15 02:38:33 -0700

    wasm support

--- build.zig
+++ build.zig
@@ -1,9 +1,11 @@
 const std = @import("std");
+const builtin = @import("builtin");
+const emcc = @import("src/emcc.zig");
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -13,78 +15,89 @@ pub fn build(b: *std.Build) void {
     // Standard optimization options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
-    const optimize = std.builtin.OptimizeMode.ReleaseFast;
+    const optimize = b.standardOptimizeOption(.{});
 
-    const exe_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // This creates another `std.Build.Step.Compile`, but this one builds an executable
-    // rather than a static library.
-    const exe = b.addExecutable(.{
-        .name = "image_viewer",
-        .root_module = exe_mod,
-    });
     const imglib = b.dependency("imglib", .{});
-    exe.root_module.addImport("image", imglib.module("image"));
     const termlib = b.dependency("terminal", .{});
-    exe.root_module.addImport("term", termlib.module("term"));
     const commonlib = b.dependency("common", .{});
-    exe.root_module.addImport("common", commonlib.module("common"));
     const zigxel_lib = b.dependency("engine", .{});
     zigxel_lib.module("engine").addImport("image", imglib.module("image"));
     zigxel_lib.module("engine").addImport("term", termlib.module("term"));
     zigxel_lib.module("engine").addImport("common", commonlib.module("common"));
-    exe.root_module.addImport("engine", zigxel_lib.module("engine"));
-    exe.linkLibC();
 
-    exe.linkLibrary(zigxel_lib.artifact("libzigxel"));
+    if (target.result.os.tag == .emscripten) {
+        const wasm_mod = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        wasm_mod.addImport("image", imglib.module("image"));
+        wasm_mod.addImport("term", termlib.module("term"));
+        wasm_mod.addImport("common", commonlib.module("common"));
+        wasm_mod.addImport("engine", zigxel_lib.module("engine"));
+        _ = try emcc.Build(b, null, wasm_mod, target, optimize, b.path("src/main.zig"), null, "src/shell.html", null);
+    } else {
+        const exe_mod = b.createModule(.{
+            // `root_source_file` is the Zig "entry point" of the module. If a module
+            // only contains e.g. external object files, you can make this `null`.
+            // In this case the main source file is merely a path, however, in more
+            // complicated build scripts, this could be a generated file.
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        const exe = b.addExecutable(.{
+            .name = "image_viewer",
+            .root_module = exe_mod,
+        });
+        exe.root_module.addImport("image", imglib.module("image"));
+        exe.root_module.addImport("term", termlib.module("term"));
+        exe.root_module.addImport("common", commonlib.module("common"));
+        exe.root_module.addImport("engine", zigxel_lib.module("engine"));
+        exe.linkLibC();
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
+        exe.linkLibrary(zigxel_lib.artifact("libzigxel"));
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
+        // This declares intent for the executable to be installed into the
+        // standard location when the user invokes the "install" step (the default
+        // step when running `zig build`).
+        b.installArtifact(exe);
 
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
+        // This *creates* a Run step in the build graph, to be executed when another
+        // step is evaluated that depends on it. The next line below will establish
+        // such a dependency.
+        const run_cmd = b.addRunArtifact(exe);
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        // By making the run step depend on the install step, it will be run from the
+        // installation directory rather than directly from within the cache directory.
+        // This is not necessary, however, if the application depends on other installed
+        // files, this ensures they will be present and in the expected location.
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        // This allows the user to pass arguments to the application in the build
+        // command itself, like this: `zig build run -- arg1 arg2 etc`
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        // This creates a build step. It will be visible in the `zig build --help` menu,
+        // and can be selected like this: `zig build run`
+        // This will evaluate the `run` step rather than the default, which is "install".
+        const run_step = b.step("run", "Run the app");
+        run_step.dependOn(&run_cmd.step);
+
+        const exe_unit_tests = b.addTest(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+
+        const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+        // Similar to creating the run step earlier, this exposes a `test` step to
+        // the `zig build --help` menu, providing a way for the user to request
+        // running the unit tests.
+        const test_step = b.step("test", "Run unit tests");
+        test_step.dependOn(&run_exe_unit_tests.step);
     }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const image = @import("image");
 const term = @import("term");
 const engine = @import("engine");
@@ -8,7 +9,7 @@ pub const Image = image.Image;
 pub const Graphics = engine.Graphics;
 
 pub const Error = error{} || engine.Error || term.Error || Image.Error;
-
+var allocator: std.mem.Allocator = undefined;
 pub const std_options: std.Options = .{
     .log_level = .err,
     .logFn = myLogFn,
@@ -35,53 +36,70 @@ pub fn myLogFn(
     nosuspend stderr.print(prefix ++ format, args) catch return;
 }
 
-pub fn render_img(img: *Image, allocator: std.mem.Allocator) Error!void {
-    var g: Graphics = try Graphics.init(allocator, .pixel, ._2d, .color_true);
+export fn render_img(name: [*:0]const u8, len: usize) usize {
+    const name_slice: []const u8 = name[0..len];
+    const extension: []const u8 = name_slice[len - 3 ..];
+    var img: Image = undefined;
+    if (std.mem.eql(u8, extension, "jpg")) {
+        img = Image.init_load(allocator, name_slice, .JPEG) catch {
+            return 1;
+        };
+    } else if (std.mem.eql(u8, extension, "bmp")) {
+        img = Image.init_load(allocator, name_slice, .BMP) catch {
+            return 1;
+        };
+    } else if (std.mem.eql(u8, extension, "png")) {
+        img = Image.init_load(allocator, name_slice, .PNG) catch {
+            return 1;
+        };
+    } else {
+        std.log.info("Image must be .jpg/.png/.bmp\n", .{});
+    }
+    defer img.deinit();
+    var g: Graphics = Graphics.init(allocator, .pixel, ._2d, .color_true, .wasm) catch {
+        return 1;
+    };
     const ratio = @as(f32, @floatFromInt(img.width)) / @as(f32, @floatFromInt(img.height));
-    const height = @as(u32, @intCast(g.pixel.terminal.size.height));
-    const width = @as(u32, @intFromFloat(@as(f32, @floatFromInt(g.pixel.terminal.size.height)) * ratio));
-    std.log.info("width {d}, height {d}, width {d}, height {d} \n", .{ img.width, img.height, width, height });
-    const pixels = try image.image_core.bilinear(allocator, img.data.items, img.width, img.height, width, height);
+    const height = @as(u32, @intCast(g.pixel.pixel_height));
+    const width = @as(u32, @intFromFloat(@as(f32, @floatFromInt(g.pixel.pixel_height)) * ratio));
+    const pixels = image.image_core.bilinear(allocator, img.data.items, img.width, img.height, width, height) catch {
+        return 1;
+    };
     defer allocator.free(pixels);
     g.pixel.first_render = false;
-    try g.pixel.draw_pixel_buffer(pixels, width, height, .{ .x = 0, .y = 0, .width = width, .height = height }, .{ .x = 0, .y = 0, .width = width, .height = height }, null);
-    try g.pixel.flip(null, null);
-    _ = try g.pixel.terminal.stdin.readByte();
-    try g.deinit();
+    g.pixel.draw_pixel_buffer(pixels, width, height, .{ .x = 0, .y = 0, .width = width, .height = height }, .{ .x = 0, .y = 0, .width = width, .height = height }, null) catch {
+        return 1;
+    };
+    g.pixel.flip(null, null) catch {
+        return 1;
+    };
+    if (builtin.os.tag != .emscripten) {
+        _ = g.pixel.terminal.stdin.readByte() catch {
+            return 1;
+        };
+    }
+    g.deinit() catch {
+        return 1;
+    };
+    return 0;
 }
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
+    if (builtin.os.tag == .emscripten) {
+        allocator = std.heap.c_allocator;
+    } else {
+        allocator = gpa.allocator();
+    }
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
     const argsv = try std.process.argsAlloc(allocator);
     if (argsv.len > 1) {
         if (argsv.len >= 2) {
-            if (argsv[1].len < 3) {
-                try stdout.print("Image must be .jpg/.png/.bmp\n", .{});
-                try bw.flush();
-                return;
-            } else {
-                const extension = argsv[1][argsv[1].len - 3 ..];
-                if (std.mem.eql(u8, extension, "jpg")) {
-                    var im = try Image.init_load(allocator, argsv[1], .JPEG);
-                    try render_img(&im, allocator);
-                    im.deinit();
-                } else if (std.mem.eql(u8, extension, "bmp")) {
-                    var im = try Image.init_load(allocator, argsv[1], .BMP);
-                    try render_img(&im, allocator);
-                    im.deinit();
-                } else if (std.mem.eql(u8, extension, "png")) {
-                    var im = try Image.init_load(allocator, argsv[1], .PNG);
-                    try render_img(&im, allocator);
-                    im.deinit();
-                } else {
-                    try stdout.print("Image must be .jpg/.png/.bmp\n", .{});
-                    try bw.flush();
-                }
-            }
+            const dupe = try allocator.dupeZ(u8, argsv[1]);
+            defer allocator.free(dupe);
+            _ = render_img(dupe, argsv[1].len);
         } else {
             try stdout.print("Usage: {s} image_file\n", .{argsv[0]});
             try bw.flush();
@@ -90,7 +108,7 @@ pub fn main() !void {
 
     std.process.argsFree(allocator, argsv);
 
-    if (gpa.deinit() == .leak) {
+    if (builtin.os.tag != .emscripten and gpa.deinit() == .leak) {
         std.log.warn("Leaked!\n", .{});
     }
 }
